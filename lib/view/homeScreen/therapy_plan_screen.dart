86b632d7d88a121caedccd5b65dfca3c640bbf8a
dharakarad <dhara.madvise@gmail.com>
2024-04-30 16:31:28 +0530

    init

--- lib/view/homeScreen/therapy_plan_screen.dart
+++ lib/view/homeScreen/therapy_plan_screen.dart
@@ -71,7 +71,7 @@ class _TherapyPlanState extends State<TherapyPlan> {
                   Row(
                     children: [
                       Container(
-                        height: ,
+                        height: 56,
                       )
                     ],
                   )
